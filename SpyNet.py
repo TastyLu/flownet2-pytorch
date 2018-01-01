90b6995c7285ce7bb9d6a3bacb68320bd68992dc
TastyLu <l295439753@163.com>
2018-01-01 15:13:15 +0800

    Add files via upload

---
+++ SpyNet.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+
+import math
+import numpy as np
+
+from submodules import *
+
+
+class SpyNet(nn.Module):
+
+    def __init__(self, args):
+        super(SpyNet, self).__init__()
+
+        self.conv1 = nn.Conv2d(8, 32, kernel_size=7, stride=1, padding=3)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=7, stride=1, padding=3)
+        self.relu2 = nn.ReLU()
+        self.conv3 = nn.Conv2d(64, 32, kernel_size=7, stride=1, padding=3)
+        self.relu3 = nn.ReLU()
+        self.conv4 = nn.Conv2d(32, 16, kernel_size=7, stride=1, padding=3)
+        self.relu4 = nn.ReLU()
+        self.conv5 = nn.Conv2d(16, 2, kernel_size=7, stride=1, padding=3)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                if m.bias is not None:
+                    init.uniform(m.bias)
+                init.xavier_uniform(m.weight)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.conv4(x)
+        x = self.relu4(x)
+        out = self.conv5(x)
+
+        return out
+
